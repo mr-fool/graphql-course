4d3d972f1cbab2c5448e55daf19de7f0a503cee6
mr-fool <mr-fool@users.noreply.github.com>
2020-02-08 17:40:34 -0700

    cleaning up

--- graphql-basics/src/index.js
+++
@@ -1,9 +0,0 @@
-import myCurrentLocation, { message, name,getGreeting } from './myModule'
-console.log(message);
-console.log(name);
-console.log(myCurrentLocation);
-console.log(getGreeting("cyka"));
-
-import myAdd, {subtract} from './math'
-console.log(myAdd(1,2));
-console.log(subtract(3,1));
