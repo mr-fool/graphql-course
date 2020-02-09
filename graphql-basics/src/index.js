bcadac07e9cfc71c3bc6d99e05a84d4c3dce442d
mr-fool <mr-fool@users.noreply.github.com>
2020-02-09 12:29:55 -0700

    set up our graphql api

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,5 +1,24 @@
 import { GraphQLServer } from "graphql-yoga"
 
 //Type definitions
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`
 
 //Resolvers
+const resolvers = {
+    Query: {
+        hello() {
+            return "This is my first query"
+        }
+    }
+}
+const server = new GraphQLServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers
+})
+server.start(() => {
+    console.log("The server is up")
+})
